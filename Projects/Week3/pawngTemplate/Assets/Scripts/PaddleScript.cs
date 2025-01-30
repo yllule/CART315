cad1586c2170d6a3ea768fcd40503ef98825cba0
yllule <catherinezaloshnja@gmail.com>
2025-01-30 18:19:02 -0500

    Pawng template

---
+++ Projects/Week3/pawngTemplate/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    private float     yPos;
+    public float      paddleSpeed = .05f;
+	
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.A)) {
+                yPos -= paddleSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Q)) {
+                yPos += paddleSpeed;
+        }
+
+        transform.localPosition = new Vector3(transform.position.x, yPos, 0);
+    }
+}
+
